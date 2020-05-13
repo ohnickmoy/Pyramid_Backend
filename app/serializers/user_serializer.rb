8b22d4139e637ead8f2c6a018f71fcdeaa5e097e
Nicholas Moy <nicksmoy@gmail.com>
2020-05-13 15:29:39 -0400

    updated user serializer to omit users workouts from login

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,34 +2,34 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :username
   #has_many :workouts
-  attributes :workouts do |object|
-    workouts = Workout.all.select do |workout|
-      workout.user_id === object.id
-    end
+  # attributes :workouts do |object|
+  #   workouts = Workout.all.select do |workout|
+  #     workout.user_id === object.id
+  #   end
 
-    workouts = workouts.map do |workout|
-      workoutExercises = WorkoutExercise.all.select do |workoutExercise|
-        workout.id === workoutExercise.workout_id
-      end
-      workoutExercises = workoutExercises.sort_by {|workoutExercise| workoutExercise.tier}
+  #   workouts = workouts.map do |workout|
+  #     workoutExercises = WorkoutExercise.all.select do |workoutExercise|
+  #       workout.id === workoutExercise.workout_id
+  #     end
+  #     workoutExercises = workoutExercises.sort_by {|workoutExercise| workoutExercise.tier}
       
-      workoutExercises = workoutExercises.map do |workoutExercise|
-        exercise = Exercise.find(workoutExercise.exercise_id)
+  #     workoutExercises = workoutExercises.map do |workoutExercise|
+  #       exercise = Exercise.find(workoutExercise.exercise_id)
 
-        workoutExercise = {
-          id: workoutExercise.id, 
-          exercise_id: workoutExercise.exercise_id,
-          workout_id: workoutExercise.workout_id,
-          name: exercise.name,
-          tier: workoutExercise.tier,
-          weight: workoutExercise.weight,
-          reps: workoutExercise.reps,
-          numSets: workoutExercise.numSets,
-          totalReps: workoutExercise.totalReps,
-          setInfo: workoutExercise.setInfo
-        }
-      end
-      workout = {id:workout.id, user_id: workout.user_id, routine_type: workout.routine_type, workout_date: workout.workout_date, exercises: workoutExercises}
-    end
-  end
+  #       workoutExercise = {
+  #         id: workoutExercise.id, 
+  #         exercise_id: workoutExercise.exercise_id,
+  #         workout_id: workoutExercise.workout_id,
+  #         name: exercise.name,
+  #         tier: workoutExercise.tier,
+  #         weight: workoutExercise.weight,
+  #         reps: workoutExercise.reps,
+  #         numSets: workoutExercise.numSets,
+  #         totalReps: workoutExercise.totalReps,
+  #         setInfo: workoutExercise.setInfo
+  #       }
+  #     end
+  #     workout = {id:workout.id, user_id: workout.user_id, routine_type: workout.routine_type, workout_date: workout.workout_date, exercises: workoutExercises}
+  #   end
+  # end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  config.hosts << 'ba64c319.ngrok.io'
+  config.hosts << '148cfa58.ngrok.io'
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
